929ad854caacb2b3b4f68090b6c640dd83c9d02f
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-02-06 09:16:25 +0300

    Добавьте файлы проекта.

---
+++ Lab3(Maksim50322_3)/MainProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Lab3_Maksim50322_3_
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
+            CultureInfo.CurrentUICulture = new CultureInfo("ru-RU");
+
+
+        }
+    }
+}
