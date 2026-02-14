e25011d8bec330ed3fdab8808f589ebe9b7953a0
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-02-15 00:24:16 +0300

    added comments and deconstructed the tuple

--- Lab3(Maksim50322_3)/MainProgramL3_1.cs
+++ Lab3(Maksim50322_3)/MainProgramL3_1.cs
@@ -10,8 +10,8 @@ namespace Lab3_Maksim50322_3_
 {
     public class MainProgramL3_1
     {
-        public delegate bool UniTryParse<T>(string input, out T value);
-        public static T ReadValue<T>(string message, UniTryParse<T> parser)
+        public delegate bool UniTryParse<T>(string input, out T value); //дженерик для readvalue<T>
+        public static T ReadValue<T>(string message, UniTryParse<T> parser) //самый универсальный метод ввода и безопасный на данный момент который я знаю
         {
             Console.Write(message);
             T value;
@@ -23,7 +23,7 @@ namespace Lab3_Maksim50322_3_
             return value;
         }
 
-        public static int ReadInt(string message)
+        public static int ReadInt(string message) //безопасный метод для int значений
         {
             Console.Write(message);
             int value;
@@ -34,23 +34,23 @@ namespace Lab3_Maksim50322_3_
             return value;
         }
 
-        public static bool IsLeapYear(int year)
+        public static bool IsLeapYear(int year) //проверка года на високосность
         {
             return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
         }
-        public static void ValidateDay(int day, bool isLeap) 
+        public static void ValidateDay(int day, bool isLeap) //выбор массива дней для года
         {
             int max = isLeap ? 366 : 365; 
-            if (day < 1 || day > max) throw new ArgumentOutOfRangeException("Day out of range");
+            if (day < 1 || day > max) throw new ArgumentOutOfRangeException("Day out of range"); //проверка корректности входных данных и генерация исключения, если данные неверные.
         }
 
-        public static (int monthNum, int dayInMonth) GetMonthAndDay(int dayNum, bool isLeap)
+        public static (int monthNum, int dayInMonth) GetMonthAndDay(int dayNum, bool isLeap) //основной расчет, на выходе кортедж данных (месяц и день) на вход (день и високостность года)
         {
-            int[] months = isLeap ? WhatDay.Program.DaysInMonthsLeap : WhatDay.Program.DaysInMonths;
+            int[] months = isLeap ? WhatDay.Program.DaysInMonthsLeap : WhatDay.Program.DaysInMonths; //данные берутся из другого проекта
             int month = 0;
             foreach (int days in months)
             {
-                if (dayNum <= days) return (month, dayNum);
+                if (dayNum <= days) return (month, dayNum); //основной вывод метода
                 dayNum -= days;
                 month++;
             }
@@ -60,23 +60,26 @@ namespace Lab3_Maksim50322_3_
 
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8; //эти три строчки для корректного ввода кирилицы
             CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
             CultureInfo.CurrentUICulture = new CultureInfo("ru-RU");
             try
             {
                 //int yearNum = ReadInt("Введите год: ");
-                int yearNum = ReadValue<int>("Введите год: ", int.TryParse);
+                int yearNum = ReadValue<int>("Введите год: ", int.TryParse); //парсим год универсальным методом
                 bool isLeapYear = IsLeapYear(yearNum);
 
                 //int dayNum = ReadInt("Введите день:");
-                int dayNum = ReadValue<int>("Введите день: ", int.TryParse);
+                int dayNum = ReadValue<int>("Введите день: ", int.TryParse); //парсим день универсальным методом
 
-                ValidateDay(dayNum, isLeapYear);
-                var result = GetMonthAndDay(dayNum, isLeapYear);
-                MonthName monthName = (MonthName)result.monthNum;
+                ValidateDay(dayNum, isLeapYear); //проверка дня на правильность, что бы чуть что завершить программу
+                //var result = GetMonthAndDay(dayNum, isLeapYear); //метод который выдает кортедж данных на входе день и високосность года
+                var (monthNum, dayInMonth) = GetMonthAndDay(dayNum, isLeapYear);
+                //MonthName monthName = (MonthName)result.monthNum;
+                MonthName monthName = (MonthName)monthNum;
                 if (isLeapYear) Console.WriteLine("Високосный год, так... на минуточку.");
-                Console.WriteLine($"{result.dayInMonth} {monthName}");
+                //Console.WriteLine($"{result.dayInMonth} {monthName}");
+                Console.WriteLine($"{dayInMonth} {monthName}");
                 Console.ReadLine();
             }catch (Exception caught)
             {
