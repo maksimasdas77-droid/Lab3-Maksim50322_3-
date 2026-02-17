44a9fbfde29ce62f3beee4ada6ecbbd7e8d734ac
Максим Дмитрачёнок <maksimasdas77@gmail.com>
2026-02-17 20:40:04 +0300

    Add new project

---
+++ WhatDayWithNewKnowledge/ProgDateTime.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDayWithNewKnowledge
+{
+
+    internal class ProgDateTime
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run)
+            {
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Введите 0 для выхода из программы");
+                    int yearnum = Readclas.ReadValue<int>("Введите год: ", int.TryParse);
+                    if (yearnum == 0)
+                    {
+                        Console.WriteLine("Выход");
+                        run = false;
+                        Console.ReadLine();
+                        break;
+                    }
+                    int daynum = Readclas.ReadValue<int>("Введите день: ", int.TryParse);
+                    bool isLeap = DateTime.IsLeapYear(yearnum);
+
+                    if (daynum < 1 || daynum > (isLeap ? 366 : 365))
+                    {
+                        Console.WriteLine("Ошибка: такого дня в этом году нет. Попробуйте снова.\n");
+                        continue;
+                    }
+
+                        //throw new ArgumentOutOfRangeException("Day out of range");
+                    DateTime date = new DateTime(yearnum, 1, 1).AddDays(daynum - 1);
+
+                    if (isLeap) Console.WriteLine("ВИСОКОСНЫЙ ГОД");
+
+                    Console.WriteLine($"\nРезультаты:");
+                    Console.WriteLine($"стардартный: {date:d MMMM yyyy}");
+                    Console.WriteLine($"имя месяца: {date:MMMM}");
+                    Console.WriteLine($"день месяца: {date.Day}");
+                    Console.WriteLine($"номер месяца: {date.Month}");
+                    Console.WriteLine($"день недели: {date.DayOfWeek}");
+                    Console.WriteLine("еще пару вариантов вывода");
+                    Console.WriteLine($"{date.Day} {date.ToString("MMMM")}");
+                    Console.WriteLine(date.ToString("dd MMMM yyyy"));
+
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
---
+++ WhatDayWithNewKnowledge/Readclas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDayWithNewKnowledge
+{
+    public class Readclas
+    {
+        public delegate bool UniTryParse<T>(string input, out T value); //дженерик для readvalue<T>
+        public static T ReadValue<T>(string message, UniTryParse<T> parser) //самый универсальный метод ввода и безопасный на данный момент который я знаю
+        {
+            Console.Write(message);
+            T value;
+
+            while (!parser(Console.ReadLine(), out value)) //цикл для зацикливания ввода если введены не правильные данные и не выдает ошибку
+            {
+                Console.Write("Некорректный ввод. Попробуйте снова: ");
+            }
+            return value;
+        }
+        public static T ReadValue<T>(UniTryParse<T> parser) //самый универсальный метод ввода и безопасный на данный момент который я знаю
+        {
+            T value;
+
+            while (!parser(Console.ReadLine(), out value)) //цикл для зацикливания ввода если введены не правильные данные и не выдает ошибку
+            {
+                Console.Write("Некорректный ввод. Попробуйте снова: ");
+            }
+            return value;
+        }
+
+        public static int ReadInt(string message) //безопасный метод для int значений в коде практически не используется
+        {
+            Console.Write(message); //текстовое значение которое мы вводим, мы вводим строку, потом преобразуем ее в INT в переменную value
+            int value; //вот сюда
+            while (!int.TryParse(Console.ReadLine(), out value)) //цикл для зацикливания ввода если введены не правильные данные и не выдает ошибку
+            {
+                Console.Write("Некорректный ввод. Попробуйте снова: "); //сообщение если введен не тот тип данных
+            }
+            return value; //значение которое возвращает метод
+        }
+
+    }
+}
